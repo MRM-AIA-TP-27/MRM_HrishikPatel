dd26d9fe6f5508273544a2a963a3cfde5867f56d
Hrishik Patel <hrishikpatel2307@gmail.com>
2025-12-23 17:51:56 +0530

    ROS@ task1 without yt link

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MRM_HrishikPatel
+
+C++ course folder contains the certifications of my cpp course
+
+GIT_HUB_TASKS contains the tasks assigned for git hub
+
+OPENCV contains all the opencv tasks  assigned befor ros2 tasks
+
+Reports contains sensor reports and the ros report
+
+ROS_tasks contains tasks for ROS@ Humble
---
+++ ROS_tasks/mrm_tp_1/launch/chat_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mrm_tp_1',
+            executable='talker_node',
+            name='talker'
+        ),
+        Node(
+            package='mrm_tp_1',
+            executable='listener_node',
+            name='listener'
+        )
+    ])
